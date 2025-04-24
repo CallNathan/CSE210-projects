2ea4146304caf8567b1787c51b422446d96d0051
CallNathan <n8.t.call@gmail.com>
2025-04-24 13:00:00 -0600

    Prep1 Completion First name last name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        String firstName = "";
+        String lastName = "";
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.WriteLine("What is your first name?");
+        firstName = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        lastName = Console.ReadLine();
+
+        Console.Write("Your name is " + lastName + ", " + firstName + " " + lastName); 
     }
 }
