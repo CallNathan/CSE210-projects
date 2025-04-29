f572cbc78f25c4f4be4a3c3064b082c50d25ac73
CallNathan <n8.t.call@gmail.com>
2025-04-29 13:29:03 -0600

    made changes to prep3 to follow stretch challenge.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -12,9 +12,12 @@ class Program
         int randomNumber = randomGen.Next(1,1000);
         string guess = "0";
         int guesses = 0;
+        string again = "yes";
+        string againNormal = "yes";
 
         Console.WriteLine("I have picked a random number between 1 and 1000. how long till you guess?");
-
+        while (againNormal == "yes")
+        {
         while (int.Parse(guess) != randomNumber) 
         {
             
@@ -39,8 +42,10 @@ class Program
                 Console.WriteLine("an error has occured.");
             }
         }
-        
-
+        Console.WriteLine("want to play again? ");
+        again = Console.ReadLine();
+        againNormal = again.Trim().ToLower();
+        }
     }
 
 }
